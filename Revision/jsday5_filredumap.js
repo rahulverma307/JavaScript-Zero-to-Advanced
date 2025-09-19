e771fb4907f9663c117b7ab7a69e06b858cf2137
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-09-19 19:10:39 +0000

    filmapreduce

---
+++ Revision/jsday5_filredumap.js
@@ -0,0 +1,10 @@
+// console.log("jia shree ram");
+
+////filter
+let  nums=[1,2,,4,5,6,7,8,9,10]
+// let newnums=nums.filter((num)=>num>5)
+let newnums= nums.filter((num)=>{
+    return num>4
+})
+console.log(newnums);
+
