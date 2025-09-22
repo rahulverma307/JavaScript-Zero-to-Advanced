fdd8e038fd20818d638225e52974d081c6a83d00
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-09-22 09:49:42 +0000

    dom script file

---
+++ Dom_bom/script.js
@@ -0,0 +1,63 @@
+// console.log("jia shree ram");
+// console.log(window);//bom topic
+
+// console.log(document.getElementsByTagName("h1"));
+//DOM Part=2
+//challenges
+  
+//Example 1\=
+/*1st method
+let hold =document.getElementById("changeTextButton")
+console.log(hold);
+*/
+//example=>change pragraph content
+// method 2 use addEventListener add
+
+document.getElementById("changeTextButton").addEventListener('click', function (){
+    let paragraph=document.getElementById("myParagraph")
+    console.log(paragraph);
+//    console.log(paragraph.textContent);// this print only paragraph thing
+ paragraph.textContent="my name is rahul verma "// this change the paragraph content
+    
+})
+
+//example 2 highlight the first element of ul
+
+document.getElementById("highlightFirstCity").addEventListener('click', function() {
+  let cities = document.getElementById("citiesList");
+//   console.log(cities.firstElementChild.classList);
+cities.firstElementChild.classList.add("highlight")
+console.log(cities.firstElementChild.classList.add("highlight")); 
+});
+
+//example 3
+// change order latte into Espresso and change color
+document.getElementById("changeOrder").addEventListener("click",function(){
+  let coffeType=document.getElementById("coffeType")
+  coffeType.textContent="Espresso"
+  // coffeType.classList.add("highlight") change color in yellow and other method is
+  coffeType.style.backgroundColor="brown" // change Espresso Color
+  coffeType.style.padding="5px"// change Padding
+  console.log(coffeType);
+})
+
+//Example 4
+// add new elemnt in the listt
+document.getElementById("addNewitem").addEventListener("click",function(){
+   let newItem=document.createElement('li')
+   newItem.textContent="Eggs"
+   document.getElementById("ShoppingList").appendChild(newItem)
+})
+
+//Example 5
+// remove Element in the list
+document.getElementById("removeLastTask").addEventListener("click",function(){
+let task= document.getElementById("tasklist")
+task.lastElementChild.remove()// remove last elemet
+})
+
+
+
+
+
+ 
