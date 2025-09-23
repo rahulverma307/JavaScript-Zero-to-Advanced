270feac79ab5a73795105063d9acbe2a3c5dd3cd
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-09-23 17:31:37 +0000

    asynandevent

---
+++ advanced_JS/1_asyn&eventloop.js
@@ -0,0 +1,22 @@
+// // console.log("jai Shree Ram");
+// function sayHello(name="prey lord shiva"){
+//     console.log(`my name is rahul verma :${name}`);
+// }
+// setTimeout(() => {
+//     sayHello()
+// }, 4000);//after two second setTimeout print =>my name is rahul verma :prey lord shiv
+
+// for (let index = 0; index < 5; index++) {
+//     const element = index*2;
+//     console.log(element)  // this for print before setTimeout   
+// }
+//  setTimeout(() => {
+//     console.log("tesla is good car");
+    
+//  }, 10000);
+
+ setInterval(() => {
+    console.log("meri 20 lakh rupey per month ki job lagegi gi december end tak 2025");
+    
+ }, 8000);
+
