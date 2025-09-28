97cb54dd6449c43b5ae70044cc40f2982f320418
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-09-28 07:36:32 +0000

    todo

--- 4_Project JS/1_Todo/app.js
+++ 4_Project JS/1_Todo/app.js
@@ -1,8 +1,10 @@
+document.addEventListener("DOMContentLoaded",()=>{
 const todoInput=document.getElementById("todo-input")
 const addTaskButton=document.getElementById("add-task-btn")
 const todoList=document.getElementById("todo-list")
 
-let tasks=[]
+let tasks=JSON.parse(localStorage.getItem('tasks') ) ||   [];
+tasks.forEach(task =>renderTasks(task));
 
 addTaskButton.addEventListener("click",function(){
    const taskText= todoInput.value.trim()
@@ -14,7 +16,19 @@ addTaskButton.addEventListener("click",function(){
     completed:false
    }
    tasks.push(newTask)
+   saveTasks()
    todoInput.value=""//for clear input
    console.log(tasks);
    
+});
+
+function renderTasks(tasks){
+   console.log(tasks.text);
+   
+}
+
+function saveTasks(){
+   localStorage.setItem("tasks",JSON.stringify(tasks))
+}
+
 })
