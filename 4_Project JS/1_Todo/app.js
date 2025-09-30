d9dc55986f9edced03cf1c5243c3def71ab4b2cb
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-09-30 04:20:04 +0000

    todo app

--- 4_Project JS/1_Todo/app.js
+++ 4_Project JS/1_Todo/app.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const todoInput = document.getElementById("todo-input");
   const addTaskButton = document.getElementById("add-task-btn");
-  const todoList = document.getElementById("todo-list");
+  const todoList = document.getElementById("todo_List");
 
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
   // Render saved tasks on load
-  tasks.forEach(task => renderTask(task));
+  tasks.forEach(task => renderTasks(task));
 
   // Add new task
   addTaskButton.addEventListener("click", function () {
@@ -21,27 +21,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     tasks.push(newTask);
     saveTasks();
-    renderTask(newTask);  // 👈 display new task
+    renderTasks(newTask); // 👈 display new task
     todoInput.value = ""; // clear input
   });
 
   // Render function
-  function renderTask(task) {
-    const li = document.createElement("li");
-    li.setAttribute("data-id", task.id);
-    li.innerHTML = `
-      <span>${task.text}</span>
-      <button class="delete-btn">Delete</button>
-    `;
+  function renderTasks(task) {
+   const li=document.createElement('li')
+   if(task.completed) li.classList.add('completed')
+   li.setAttribute('data-id',task.id)
+   li.innerHTML=`
+   <span>${task.text}</span>
+   <button>delete</button>
+   `;
 
-    // Delete button functionality
-    li.querySelector(".delete-btn").addEventListener("click", () => {
-      tasks = tasks.filter(t => t.id !== task.id); // remove from array
-      saveTasks();
-      li.remove(); // remove from UI
-    });
+   li.addEventListener('click',(e)=>{
+     if(e.target.tagName==="BUTTON") return;
+     task.completed=!task.completed
+     li.classList.toggle('completed')
+     saveTasks()
+   })
 
-    todoList.appendChild(li);
+   li.querySelector('button').addEventListener('click',(e)=>{
+    e.stopPropagation();//prevent  toggle from 
+    tasks=tasks.filter(t=>t.id !==task.id)
+    li.remove();
+    saveTasks();
+   })
+
+   todoList.appendChild(li)
+    // Later you can append it to todoList
   }
 
   // Save tasks to localStorage
